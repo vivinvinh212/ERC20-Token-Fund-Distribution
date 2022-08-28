c0e9f709d64c6058fab10f02b8198362550fa15a
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-08-28 14:51:52 +0700

    Update User.sol

--- packages/hardhat/contracts/User.sol
+++ packages/hardhat/contracts/User.sol
@@ -21,9 +21,24 @@ contract User is IUser, Ownable {
     function setAllocations(
         uint8[] memory _ratios,
         address[] memory _recipients
-    ) public onlyOwner {}
+    ) public onlyOwner {
+        require(
+            0 <= _recipients.length && _recipients.length <= 256,
+            "Recipients length is not appropriate"
+        );
+        require(
+            0 <= _ratios.length && _ratios.length <= 256,
+            "Ratios length is not appropriate"
+        );
+        require(
+            _ratios.length == recipients.length,
+            "Length of two sequences do not match!"
+        );
+    }
 
-    function getRecipientsLength() public view returns (uint) {}
+    function getRecipientsLength() public view returns (uint) {
+        return recipients.length;
+    }
 
     // to support receiving ETH by default
     receive() external payable {}
