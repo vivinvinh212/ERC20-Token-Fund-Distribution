35c3bd2dbc60451558a89fbc29002b51cd3ddece
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-08-28 14:14:06 +0700

    Update contract frame

--- packages/hardhat/contracts/User.sol
+++ packages/hardhat/contracts/User.sol
@@ -2,26 +2,26 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IUser.sol";
 
-contract YourContract {
+contract User is IUser, Ownable {
+    event AllocationSet(address[] recipients, uint8[] allocations);
 
-  event SetPurpose(address sender, string purpose);
+    string public purpose = "Building Unstoppable Apps!!!";
 
-  string public purpose = "Building Unstoppable Apps!!!";
+    constructor() payable {}
 
-  constructor() payable {
-    // what should we do on deploy?
-  }
+    function getRecipients() public view returns (address[] memory) {}
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
-  }
+    function getAllocations() public view returns (uint8[] memory) {}
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    function setAllocations() public onlyOwner {}
+
+    function getRecipientsLength() public view returns (uint) {}
+
+    // to support receiving ETH by default
+    receive() external payable {}
+
+    fallback() external payable {}
 }
